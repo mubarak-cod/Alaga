5e3da69611c91f293888c0ea1f898bc14a2dfdae
mubarak-cod <ososanyaola8@gmail.com>
2025-08-06 15:22:37 +0100

    updated code

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./LandingPage.css";
-import alaga from "../assets/alaga2.mp4";
+import alaga from "../assets/alaga2_3.mp4";
 
 const messages = [
   "An Alaga isn't just a host – she's the soul of your celebration, guiding every moment with grace and rhythm.",
@@ -45,9 +45,31 @@ const LandingPage = () => {
           zIndex: -2,
         }}
       >
+
+        {/* <source src="../assets/alaga2.mp4" type="video/mp4" /> */}
         <source src={alaga} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+
+      {/* <video
+  autoPlay
+  loop
+  muted
+  playsInline
+  onError={(e) => console.error("Video load error:", e)}
+   style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: -2,
+        }}
+>
+  <source src="/assets/alaga2.mp4" type="video/mp4" />
+</video> */}
 
       {/* Overlay */}
       <div
